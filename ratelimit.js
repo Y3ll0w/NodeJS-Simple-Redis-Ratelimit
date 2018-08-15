6d3465caffd148521c78ca3e0912f08c9f41000c
Y3ll0w <27010973+Y3ll0w@users.noreply.github.com>
2018-08-16 00:33:23 +0200

    Update ratelimit.js

--- ratelimit.js
+++ ratelimit.js
@@ -1,8 +1,9 @@
 var redis = require("promise-redis")();
 var redisClient = redis.createClient(6379, "127.0.0.1");
 
- var reqs = 2;
- var intvall = 10000;
+//This Example Allowed 5 requests per second
+ var reqs = 5; //Numer of Allowed Requests in the Time Window
+ var intvall = 1000; //Time Window in MS
  
 async function asyncBlock() {
 	
